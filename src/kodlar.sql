33bec0b1bd1fbbed979715667304154dcc4a367f
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-07-25 09:55:42 +0300

    Update kodlar.sql

--- src/kodlar.sql
+++ src/kodlar.sql
@@ -1,240 +1,239 @@
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'KEK', 'Kalite El Kitabı');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'PR', 'Prosedürler');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'FR', 'Form');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'LS', 'Liste');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'GT', 'Görev Tanımı');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'PH', 'Proses Haritasi');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'SM', 'Şema');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'SR', 'Süreç');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'TL', 'Talimat');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOKUMANTYPE', 'PL', 'Plan');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOFTYPE', 'ONF', 'Önleyici Faaliyet');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('DOFTYPE', 'DZF', 'Düzeltici Faaliyet');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('KABULDURUMU', 'OK', 'Kabul Edildi');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('KABULDURUMU', 'NO', 'Kabul Edilmedi');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'DOC', 'Döküman');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'REV', 'Revizyonlar');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'KUL', 'Kullanıcı');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'DOF', 'Düzenleyici-Önleyici Faaliyet');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'GOR', 'Görevler');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'GRDK', 'Görev-Döküman Tanımı');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'DUY', 'Duyurular');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('CINSIYET', 'E', 'Erkek');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('CINSIYET', 'K', 'Kadın');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MEDENIDURUM', 'E', 'Evli');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MEDENIDURUM', 'B', 'Bekar');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITIMDURUMU', 'IO', 'İlköğretim Okulu');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITIMDURUMU', 'LIS', 'Lise');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITIMDURUMU', 'MYO', 'Meslek Yüksek Okulu');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITIMDURUMU', 'UNV', 'Üniversite');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITIMDURUMU', 'LSU', 'Lisans Üstü');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITIMDURUMU', 'DOK', 'Doktora');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('UNIVERSITETIPI', 'DVU', 'Devlet Üniversitesi');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('UNIVERSITETIPI', 'VKU', 'Vakıf Üniversitesi');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('UNIVERSITETIPI', 'YRD', 'YurtDışı');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '01', 'Adana');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '02', 'Adıyaman');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '03', 'Afyon');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '04', 'Ağrı');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '05', 'Amasya');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '06', 'Ankara');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '07', 'Antalya');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '08', 'Artvin');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '09', 'Aydın');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '10', 'Balıkesir');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '11', 'Bilecik');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '12', 'Bingöl');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '13', 'Bitlis');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '14', 'Bolu');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '15', 'Burdur');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '16', 'Bursa');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '17', 'Çanakkale');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '18', 'Çankırı');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '19', 'Çorum');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '20', 'Denizli');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '21', 'Diyarbakır');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '22', 'Edirne');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '23', 'Elazığ');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '24', 'Erzincan');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '25', 'Erzurum');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '26', 'Eskişehir');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '27', 'Gaziantep');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '28', 'Giresun');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '29', 'Gümüşhane');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '30', 'Hakkari');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '31', 'Hatay');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '32', 'Isparta');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '33', 'Mersin');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '34', 'İstanbul');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '35', 'İzmir');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '36', 'Kars');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '37', 'Kastamonu');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '38', 'Kayseri');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '39', 'Kırklareli');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '40', 'Kırşehir');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '41', 'Kocaeli');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '42', 'Konya');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '43', 'Kütahya');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '44', 'Malatya');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '45', 'Manisa');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '46', 'K.Maraş');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '47', 'Mardin');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '48', 'Muğla');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '49', 'Muş');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '50', 'Nevşehir');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '51', 'Niğde');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '52', 'Ordu');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '53', 'Rize');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '54', 'Sakarya');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '55', 'Samsun');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '56', 'Siirt');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '57', 'Sinop');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '58', 'Sivas');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '59', 'Tekirdağ');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '60', 'Tokat');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '61', 'Trabzon');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '62', 'Tunceli');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '63', 'Şanlıurfa');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '64', 'Uşak');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '65', 'Van');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '66', 'Yozgat');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '67', 'Zonguldak');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '68', 'Aksaray');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '69', 'Bayburt');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '70', 'Karaman');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '71', 'Kırıkkale');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '72', 'Batman');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '73', 'Şırnak');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '74', 'Bartın');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '75', 'Ardahan');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '76', 'Iğdır');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '77', 'Yalova');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '78', 'Karabük');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '79', 'Kilis');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '80', 'Osmaniye');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('ŞEHİR', '81', 'Düzce');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'EGT', 'Eğitim');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('MODUL', 'TPT', 'Toplantı ve Seminer');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITITMTYPE', 'FR', 'FORMAL');
-INSERT INTO documents_my_codes (type, code, title)
+INSERT INTO documents_mycodes (type, code, title)
             VALUES ('EGITIMTYPE', 'INF', 'INFORMAL');
 
-COMMIT WORK;
